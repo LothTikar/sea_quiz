361f4c48cf623bcfa6777225275629f927033338
LothTikar <LothTikar@gmail.com>
2018-10-04 17:40:33 -0500

    added a couple data structures and comments about program flow

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include <gtk/gtk.h>
 
+struct Question
+{
+	char *question; /* ptr to header, do not free */
+	char *answer; /* stores answer, free this when done */
+}
+
+struct Item
+{
+	int num_questions;
+	int num_hints;
+	int num_images;
+	struct Question *questions;
+	char **hints;
+	char **images; /* image filenames, give to GTK for creating image widgets via filename */
+}
+
+int num_items;
+struct Item *items;
+
+char **header; /* only copy of header, do not free until program completion */
+
 static void activate ( GtkApplication *app,
                        gpointer        user_data )
 {
@@ -11,9 +32,32 @@ static void activate ( GtkApplication *app,
     gtk_widget_show_all ( window );
 }
 
+/* program logic
+
+	Create buttons that set the label status.
+	
+	When butten is pressed for next question, create dialog if user hasn't gotten all questions right by checking labels.
+	
+*/
+
+/* function to find image names
+
+	search directory of csv file for all names that begin with image prefix
+ */
+
+/* function to process csv file
+
+	create header listing
+
+	go row by row, use header to figure out what to do
+
+	skip blanks, so there can be unused questions for items, eg rocks don't have luster but minerals do
+ */
+
 int main ( int    argc,
            char **argv )
 {
+	
     GtkApplication *app;
     int status;
 
